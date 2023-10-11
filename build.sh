cf470b20e2547ce9eff63b2788f9481368114590
Phien Nguyen <nguyenphien17@yahoo.com>
2023-10-11 00:37:33 -0700

    final

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ sudo docker build -t android_docker .
 sudo docker run -idt \
     --name=and\
     --env="DISPLAY=$DISPLAY"\
+    --env PULSE_SERVER=unix:/tmp/pulseaudio.socket \
+    --volume /tmp:/tmp \
     --env="QT_X11_NO_MITSHM=1"\
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"\
     --env="XAUTHORITY=$XAUTH"\
